cb56d8e81eea4ca648b029224c70d88d9ea28c52
Igor Prokhorov <iprokhorovster@gmail.com>
2025-03-01 20:51:53 +0300

    init

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -7,10 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MainTest {
 
     @Test
-    public void sum() throws Exception {
+    public void sum() {
         int a = 2;
         int b = 1;
 
         assertEquals(a + b, new Main().sum(1, 2));
+
+        System.out.println("Прачечная хуячечная!");
     }
 }
