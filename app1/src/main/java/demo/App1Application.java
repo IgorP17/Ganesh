30a6d14460bfbd761d3cfe5676872789180dbf6b
Igor Prokhorov <iprokhorovster@gmail.com>
2025-03-12 22:32:59 +0300

    app1 proceed

--- app1/src/main/java/demo/App1Application.java
+++ app1/src/main/java/demo/App1Application.java
@@ -3,7 +3,7 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"demo", "demo.controller"})
 public class App1Application {
     public static void main(String[] args) {
         SpringApplication.run(App1Application.class, args);
