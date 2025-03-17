d3b4cdaa8bc0092ab5f0325e1a0ca97ffd836060
Igor Prokhorov <iprokhorovster@gmail.com>
2025-03-17 23:42:21 +0300

    app1 to app3

--- app1/src/main/java/demo/controller/RequestController.java
+++ app1/src/main/java/demo/controller/RequestController.java
@@ -4,8 +4,10 @@ import demo.model.Request;
 import demo.repository.RequestRepository;
 import demo.service.KafkaConsumerService;
 import demo.service.KafkaProducerService;
+import demo.service.RequestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,6 +22,9 @@ public class RequestController {
         this.kafkaProducerService = kafkaProducerService;
     }
 
+    @Autowired
+    private RequestService requestService;
+
     @PostMapping("/send")
     public String sendRequest(@RequestBody String data) {
         logger.info("Got request: {}", data);
@@ -29,5 +34,11 @@ public class RequestController {
 
         kafkaProducerService.sendMessage("requests", request.getId(), data);
         return "Request saved with ID: " + request.getId();
+    }
+
+    // Эндпоинт для поиска запроса по ID
+    @GetMapping("/request/{id}")
+    public String getRequestData(@PathVariable Long id) {
+        return requestService.getRequestData(id);
     }
 }
---
+++ app1/src/main/java/demo/service/RequestService.java
@@ -0,0 +1,38 @@
+package demo.service;
+
+import demo.model.Request;
+import demo.repository.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RequestService {
+
+    @Autowired
+    private RequestRepository requestRepository;
+
+    // Сохраняем запрос в базу данных
+    public Long saveRequest(String message) {
+        Request request = new Request();
+        request.setData(message);
+        request.setStatus("PENDING"); // Устанавливаем начальный статус
+        requestRepository.save(request);
+        return request.getId(); // Возвращаем ID сохраненного запроса
+    }
+
+    // Ищем запрос по ID
+    public String getRequestData(Long id) {
+        Optional<Request> requestOptional = requestRepository.findById(id);
+
+        if (requestOptional.isPresent()) {
+            Request request = requestOptional.get();
+            return "Request ID: " + request.getId() + "\n" +
+                    "Message: " + request.getData() + "\n" +
+                    "Status: " + request.getStatus();
+        } else {
+            return "Request not found";
+        }
+    }
+}
